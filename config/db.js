bc38c382591e166803e66541f43a7267be7ef9b5
Sushil Singh Rathore <136253260+RSushil1@users.noreply.github.com>
2023-07-27 13:19:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
